e0b5a66a79a7e1677238699281bedfc144815b67
DavidCameron <davidc818@nycstudents.net>
2025-11-17 14:32:21 -0500

    Initial commit

---
+++ src/Hangman.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hangman {
+
+    public static String hangman(){
+        Scanner scan = new Scanner(System.in);
+        List<String> myStringList = new ArrayList<String>();
+        myStringList.add("PRIDE");
+        myStringList.add("DOUGH");
+        myStringList.add("BILL");
+        myStringList.add("COACH");
+        myStringList.add("HORSE");
+        myStringList.add("SOAR");
+        myStringList.add("LINE");
+        myStringList.add("TOP");
+        String word =
+    }
+}
